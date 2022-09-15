29a78dea15ba471eb28d92c7e6cdf12237d6b588
tommydebisi <tommydebisi@gmail.com>
2022-09-15 03:51:57 +0100

    :construction: getter set for filestorage

--- models/state.py
+++ models/state.py
@@ -3,6 +3,9 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.city import City
+import models
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -10,5 +13,17 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
-    # backref create an attribute in the city object with relationship
-    cities = relationship('City', backref='state', cascade='all, delete')
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        # backref create an attribute in the city object with relationship
+        cities = relationship('City', backref='state', cascade='all, delete')
+    else:
+        @property
+        def cities(self):
+            """ Getter attribute """
+            city_objs = models.storage.all(City)
+            relation_list = []
+            for val in city_objs.values():
+                if val.state_id == State.id:
+                    relation_list.append(val.state_id)
+
+            return relation_list
