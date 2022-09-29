738f54f80f0dbd28e4040709444a7482a926e386
tommydebisi <tommydebisi@gmail.com>
2022-09-29 13:34:31 +0100

    :seedling: if statement config

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # This script sets up your web servers for the deployment of web_static
-if ! type nginx 2>/dev/null
+if ! type nginx &>/dev/null
 then
     sudo apt-get update
     sudo apt-get install -y nginx
@@ -11,7 +11,6 @@ mkdir -p /data/web_static/releases/ /data/web_static/shared/ /data/web_static/re
 echo "created the test file" > /data/web_static/releases/test/index.html
 
 ln -sf data/web_static/releases/test/ /data/web_static/current
-
 chown -R ubuntu:ubuntu /data/
 
 sed -i "/server_name _;/a \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
