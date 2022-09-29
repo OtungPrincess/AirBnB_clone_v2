5fc4a7bbef54c93fc8cb95c6f09ae0f532194379
tommydebisi <tommydebisi@gmail.com>
2022-09-29 13:45:19 +0100

    :bug: added '/' to link

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,7 +10,7 @@ fi
 mkdir -p /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
 echo "created the test file" > /data/web_static/releases/test/index.html
 
-ln -sf data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 
 sed -i "/server_name _;/a \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
