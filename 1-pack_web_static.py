32cc8d571cbd01bd6cea60d0813274cc186ec508
tommydebisi <tommydebisi@gmail.com>
2022-09-29 14:51:45 +0100

    :rotating_light: linter issues fixed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,13 +6,14 @@
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
     """
         compresses web_static files into one file
     """
     local("mkdir -p versions")
-
-    tar_path = "versions/web_static_{}.tgz".format(datetime.now().strftime("%Y%m%d%H%M%S"))
+    current_date = datetime.now().strftime("%Y%m%d%H%M%S")
+    tar_path = "versions/web_static_{}.tgz".format(current_date)
     captured = local("tar -cvzf {} web_static".format(tar_path), capture=True)
     if captured.failed:
         return None
